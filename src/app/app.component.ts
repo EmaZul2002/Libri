d6ff6140318986a56a365691b45a1c8a60c3d469
emanuel.zulian <emanuel.zulian@mat.tn.it>
2024-01-22 11:57:44 +0100

    primo caricamento

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Libri';
+
+  visualizzaFormElenco : boolean = true
+
+  cambiaVisualizzazione() {
+    this.visualizzaFormElenco = !this.visualizzaFormElenco
+  }
 }
---
+++ src/app/elenco/elenco.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-elenco',
+  templateUrl: './elenco.component.html',
+  styleUrl: './elenco.component.css'
+})
+export class ElencoComponent {
+
+}
